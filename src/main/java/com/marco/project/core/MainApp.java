acbdcfdb22c5395e9eb39cf2fb9cecdc30353620
Marco Ricci <c.marcoricci@gmail.com>
2017-04-11 13:27:07 +0200

    Test new commit

--- src/main/java/com/marco/project/core/MainApp.java
+++ src/main/java/com/marco/project/core/MainApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// comment
+		// new comment
 		SpringApplication.run(MainApp.class, args);
 	}
 
